3e8c63ee6dcce79afe4effeb3e667638e11098bb
vinvins <vinnarasivins@gmail.com>
2024-05-28 23:14:55 +0530

    First Commit for Dapper CRUD

---
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Controllers
+{
+
+
+    public class EmployeeController : Controller
+    {
+
+
+        string connectionString = "Data source=.;Initial catalog=EmployeeDB;Integrated Security=True";
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            IEnumerable<Employee> employees;
+            using(var connection=new SqlConnection(connectionString))
+            {
+                var sql = "select * from Employee where IsActive=1";
+                employees = connection.Query<Employee>(sql);
+            }
+            return View(employees);
+        }
+    }
+}
---
+++ EmployeeManagement/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string EmailID { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
