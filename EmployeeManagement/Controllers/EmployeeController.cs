4c347d1afaba1d7ecb168b76e8fe9df14616e263
vinvins <vinnarasivins@gmail.com>
2024-05-29 00:49:00 +0530

    Employee Add functionality Implemented

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -15,19 +15,31 @@ namespace EmployeeManagement.Controllers
     public class EmployeeController : Controller
     {
 
-
-        string connectionString = "Data source=.;Initial catalog=EmployeeDB;Integrated Security=True";
-
+              
         // GET: Employee
         public ActionResult Index()
         {
-            IEnumerable<Employee> employees;
-            using(var connection=new SqlConnection(connectionString))
-            {
-                var sql = "select * from Employee where IsActive=1";
-                employees = connection.Query<Employee>(sql);
-            }
+            IEnumerable<Employee> employees = DapperORM.ReturnList<Employee>("GetEmployeeList");           
             return View(employees);
+        }
+
+        [HttpGet]
+        public ActionResult AddorEdit(int ID = 0)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddorEdit(Employee employee)
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@ID",employee.ID);
+            param.Add("@Name", employee.Name);
+            param.Add("@Age", employee.Age);
+            param.Add("@EmailID", employee.EmailID);
+            param.Add("@IsActive", employee.IsActive);
+            DapperORM.ExecuteWithoutReturn("AddOrEditEmployee", param);
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ EmployeeManagement/Models/DapperORM.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace EmployeeManagement.Models
+{
+    public class DapperORM
+    {
+        public static string ConnectionString = "Data source=.;Initial Catalog=EmployeeDB;Integrated security=True;";
+
+        public static void ExecuteWithoutReturn(string ProcedureName,DynamicParameters param = null)
+        {
+            using(SqlConnection con=new SqlConnection(ConnectionString))
+            {
+                con.Execute(ProcedureName,param,commandType:CommandType.StoredProcedure);
+            }
+        }
+
+        public static IEnumerable<T> ReturnList<T>(string ProcedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                return con.Query<T>(ProcedureName, param, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public static T ReturnScalar<T>(string ProcedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                return (T) Convert.ChangeType(con.Query(ProcedureName, param, commandType: CommandType.StoredProcedure),typeof(T));
+            }
+        }
+
+    }
+}
